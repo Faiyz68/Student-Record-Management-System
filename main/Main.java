01844e5347db177cdf1dd74a6e3b9e1cfad09351
Faiyz68 <134617866+Faiyz68@users.noreply.github.com>
2026-06-18 23:22:49 +0300

    fourth commit

--- main/Main.java
+++ main/Main.java
@@ -1,7 +1,39 @@
 package main;
+import student.Student;
+import student.StudentRecordSystem;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     void main(){
-        
+        StudentRecordSystem sys= new StudentRecordSystem();
+        ArrayList<Student> Studentrecord = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            System.out.println(" STUDENT SYSTEM ");
+            System.out.println(" 1. ADD ");
+            System.out.println(" 2. SEARCH ");
+            System.out.println(" 3. UPDATE ");
+            System.out.println("4. DELETE ");
+            System.out.println("5. REPORT ");
+            System.out.println("6. BACKUP ");
+             System.out.println("7. FILE INFO");
+             System.out.println("0. EXIT ");
+            int choice = sc.nextInt();
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }     
     }
 }
