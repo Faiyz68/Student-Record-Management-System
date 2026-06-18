ca770fc9cab90a50ee75914e0fbbb264f38b62cc
Faiyz <faiyzahmed73@gmail.com>
2026-06-19 00:26:35 +0300

    added .ser file

--- main/Main.java
+++ main/Main.java
@@ -1,7 +1,55 @@
 package main;
+import student.Student;
+import student.StudentRecordSystem;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     void main(){
-        
+        StudentRecordSystem sys= new StudentRecordSystem();
+        ArrayList<Student> Studentrecord = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            System.out.println(" STUDENT SYSTEM ");
+            System.out.println(" 1. ADD ");
+            System.out.println(" 2. SEARCH ");
+            System.out.println(" 3. UPDATE ");
+            System.out.println("4. DELETE ");
+            System.out.println("5. REPORT ");
+            System.out.println("6. BACKUP ");
+             System.out.println("7. FILE INFO");
+             System.out.println("0. EXIT ");
+            int choice = sc.nextInt();
+            if (choice == 1){
+              System.out.print("ENTER ID , NAME , DEPT , GPA : ");
+            sys.addStudent(new Student(sc.nextInt(), sc.next(), sc.next(), sc.nextDouble()));
+            }
+            else if(choice == 2){
+                 System.out.print("ENTER ID : ");
+                 System.out.println(sys.search(sc.nextInt()));
+            }
+            else if(choice == 3){
+                 System.out.print("ENTER ID TO UPDATE , NAME, DEPT, GPA : ");
+                sys.updateStudent(sc.nextInt(), sc.next(), sc.next(), sc.nextDouble());
+            }
+            else if (choice == 4){
+                 System.out.print("Enter ID to Delete : ");
+                sys.deleteStudent(sc.nextInt());
+            }
+            else if (choice == 5 ){
+                sys.generateReport();
+                 System.out.println("Report saved to report.txt");
+            }
+            else if (choice ==6){
+                sys.createBackup();
+                 System.out.println("Backup created ");
+            }
+            else if (choice == 7){
+                sys.showFileDetails();
+            }
+            else break;
+
+        }  
+        sc.close();
     }
 }
--- student/StudentRecordSystem.java
+++ student/StudentRecordSystem.java
@@ -4,5 +4,54 @@ import java.util.*;
 public class StudentRecordSystem{
     private List<Student> students=new ArrayList<>();
     private final String OBJ_FILE="students.ser";
+
+    public StudentRecordSystem(){
+        File dir = new File("StudentDate");
+        if (!dir.exists()) dir.mkdir();
+    }
+    public void addStudent (Student s) {students.add(s);  saveToFile(); }
+    public Student search(int ID){
+        for(Student s : students) if (s.getID() == ID) return s;
+        return null;
+    } 
+    public void updateStudent(int ID, String name, String department, double GPA ) {
+        Student s = search(ID);
+        if (s != null) {
+            s.setName(name); s.setDepartment(department); s.setGPA(GPA);
+            saveToFile();
+        }
+    }
+    public void deleteStudent(int ID){
+        students.removeIf(s-> s.getID()==ID);
+        saveToFile();
+    }
+    public void saveToFile(){
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(OBJ_FILE))){
+            oos.writeObject(students);
+        }catch (IOException e){System.out.println("ERROR SAVING : " + e.getMessage());
+                              }
+    }
+    public void generateReport(){
+        try (PrintWriter pw = new PrintWriter(new FileWriter("report.txt"))){
+            double sum = 0;
+            for (Student s : students ) sum+=s.getGPA();
+            pw.println("Total Students : " + students.size());
+            pw.println("Average GPA : " + (students.size()>0?sum/students.size() : 0));
+            
+        }catch (IOException e ) {e.printStackTrace();}
+    }
+    public void createBackup(){
+        try(BufferedInputStream bis = new BufferedInputStream(new FileInputStream(OBJ_FILE));
+            BufferedOutputStream bos= new BufferedOutputStream(new FileOutputStream("backup.ser"))){
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = bis.read(buffer)) > 0 ) bos.write(buffer, 0 , length);
+            } catch (IOException e) { e.printStackTrace();}
+    }
+    public void showFileDetails(){
+        File f = new File(OBJ_FILE);
+        System.out.println("File : " + f.getName() + " | path : " + f.getAbsolutePath() + " | Size : " + f.length() + " bytes");
+        
+    }    
     
 }
